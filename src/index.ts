fddfd892e447694e9316a18de33413037568bb73
azechi <azechi.n@gmail.com>
2023-03-25 13:47:21 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ interface Env {
 export default {
 	async fetch(req: Request, env: Env, _context: ExecutionContext) {
 
-		const KEY = crypto.subtle.importKey(
+		const key = await crypto.subtle.importKey(
 			"raw",
 			hexToBin(env.PUBLIC_KEY),
 			{name:"NODE-ED25519", namedCurve:"NODE-ED25519"},
@@ -18,41 +18,40 @@ export default {
 			["verify"]
 		);
 
-	const key = await KEY;
-	const sign = hexToBin(req.headers.get('x-signature-ed25519')??"");
-	const ts = new TextEncoder().encode(req.headers.get('x-signature-timestamp')??"");
+		const sign = hexToBin(req.headers.get('x-signature-ed25519')??"");
+		const ts = new TextEncoder().encode(req.headers.get('x-signature-timestamp')??"");
 
-	const body = new Uint8Array(await req.clone().arrayBuffer());
-	const data = new Uint8Array(ts.byteLength + body.byteLength);
-	data.set(ts, 0);
-	data.set(body, ts.byteLength);
+		const body = new Uint8Array(await req.clone().arrayBuffer());
+		const data = new Uint8Array(ts.byteLength + body.byteLength);
+		data.set(ts, 0);
+		data.set(body, ts.byteLength);
 
-	let result = await crypto.subtle.verify(
-		{name:"NODE-ED25519"},
-		key,
-		sign,
-		data
-	).catch(err => console.log("verify error", JSON.stringify(err)));
+		let result = await crypto.subtle.verify(
+			{name:"NODE-ED25519"},
+			key,
+			sign,
+			data
+		).catch(err => console.log("verify error", JSON.stringify(err)));
 
-	console.log(
-		await(
-			req.json()
-			.catch(_ => req.text())
-		)
-		.catch(_ => new TextDecoder().decode(body))
-	);
+		console.log(
+			await(
+				req.json()
+				.catch(_ => req.text())
+			)
+			.catch(_ => new TextDecoder().decode(body))
+		);
 
-	if (result) {
-		return new Response(JSON.stringify({
-			"type": 1
-		}), {
-			status: 200
-		});
+		if (result) {
+			return new Response(JSON.stringify({
+				"type": 1
+			}), {
+				status: 200
+			});
+		}
+
+		return new Response(null, {status: 401, statusText: "invalid request signature"});
 	}
-
-	return new Response(null, {status: 401, statusText: "invalid request signature"});
 }
-	}
 
 
 
