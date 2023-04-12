d64dc0f3fba90bbc1552a4f284fbc55cd43df5e8
azechi <azechi.n@gmail.com>
2023-04-12 09:47:03 +0900

    一重引用符でできた！

--- src/handlers/echo.ts
+++ src/handlers/echo.ts
@@ -1,4 +1,19 @@
 export default async function echoHandler(interaction: any) {
-  const content = JSON.stringify(interaction);
-  return { type: 4, data: { content } };
+  if (interaction.data.options) {
+    const s = interaction.data.options[0].value;
+    console.log(`    loptions[0].value\n---------------------`);
+    console.log(`[${s}]`);
+    console.log(`-----------------------------------------------`);
+    const data = s.slice(1,-1);
+    return `{"type": 4, "data": ${data}}`;
+  }
+
+  const payload = {
+    content: ":beer:\n```ts\nconsole.log(\"hello\");\n````test`__test__"
+  }
+
+  let str_payload = JSON.stringify(payload).replaceAll("`", "\\u0060");
+  const str_msg = JSON.stringify("`` `" + str_payload + "` ``");
+  return `{"type": 4, "data": {"embeds": [{"description": ${str_msg}}] }}`;
 }
+
