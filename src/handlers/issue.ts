c189f93d3543ad0b04b21ed73464ac43f517c6f8
azechi <azechi@users.noreply.github.com>
2023-04-14 10:05:52 +0900

    3-メッセージを埋め込みに持ったメッセージオブジェクトを作る (#17)

--- src/handlers/issue.ts
+++ src/handlers/issue.ts
@@ -3,14 +3,36 @@ export default async function issueHandler(
   { makeReturnEnvelope }: any
 ) {
   const msg = interaction.data.resolved.messages[interaction.data.target_id];
+  const {
+    content,
+    embeds,
+    author,
+    channel_id,
+    timestamp,
+    edited_timestamp,
+    id: message_id,
+  } = msg;
 
-  const { content, embeds } = msg;
+  const { guild_id } = interaction;
+  const { id: user_id, avatar: avatar_hash } = author;
 
   const payload = {
     type: 4,
     data: {
-      content,
-      embeds,
+      embeds: [
+        {
+          color: 0x4e5058,
+          thumbnail: {
+            url: `https://cdn.discordapp.com/avatars/${user_id}/${avatar_hash}.png`,
+          },
+          description: `https://discord.com/channels/${guild_id}/${channel_id}/${message_id}\n<@${user_id}>\n\n${content}`,
+          footer: {
+            text: edited_timestamp ? "(編集済み)" : "",
+          },
+          timestamp: edited_timestamp ?? timestamp,
+        },
+        ...embeds,
+      ],
     },
   };
 
@@ -21,7 +43,7 @@ export default async function issueHandler(
   return JSON.stringify({
     type: 4,
     data: {
-      content: "</post:1091535901768822915>",
+      content: "</post:1094172109409898556>",
       embeds: [{ description: token }],
       flags: 1 << 6,
     },
